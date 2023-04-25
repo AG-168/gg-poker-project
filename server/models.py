a3c9bf9f47e1ad66c9dc7bdeee2d4979df01cf68
Anson Guo <anson.guo.921104@gmail.com>
2023-04-25 15:38:42 -0400

    initalized db

--- server/models.py
+++ server/models.py
@@ -3,3 +3,7 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    
