db3986c6e479218a8a539b5aef6619ae9fa140b3
Anson Guo <anson.guo.921104@gmail.com>
2023-04-28 12:53:57 -0400

    front end setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import '../App.css';
+
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
