ffb28528818dc77033b7eb2e218f6edb62346061
Anson Guo <anson.guo.921104@gmail.com>
2023-05-01 15:26:12 -0400

    continue build

--- server/app.py
+++ server/app.py
@@ -8,8 +8,15 @@ from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-from models import User
+from models import User, Skatepark, Review
 
+class SkateparkList(Resource):
+    def get(self):
+        skateparks = Skatepark.query.all()
+        skateparks_list = [skatepark.to_dict() for skatepark in skateparks]
+        return make_response(skateparks_list, 200)
+
+api.add_resource(SkateparkList, '/api/skateparks')
 
 # Views go here!
 
